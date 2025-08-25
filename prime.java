575b79dc415fa44f8f926b5e32327e3755a2364c
ummalarajunikhitha <ummalaraju77@gmail.com>
2025-08-25 15:32:03 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,8 +1,8 @@
 public class prime{
 
     public static void main(String[] args) {
-        System.out.println("Prime numbers between 1 and 50 are:");
-        for (int i = 2; i <= 50; i++) { // Start from 2 as 1 is not prime
+        System.out.println("Prime numbers between 1 and 500 are:");
+        for (int i = 2; i <= 500; i++) { // Start from 2 as 1 is not prime
             if (isPrime(i)) {
                 System.out.print(i + " ");
             }
@@ -26,4 +26,5 @@ public class prime{
     }
 
 }
+
 
